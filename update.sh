95ac2251eb4a7dc19e5200c0bacd64cedcdf54c7
Myroslav Rys <stonevil@gmail.com>
2013-09-10 14:31:11 +0300

    Add oh-my-zsh to update procedure

--- update.sh
+++ update.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # Update vim and vim modules
+cd ~/.oh-my-zsh && git pull
+cd ~/.oh-my-zsh && git pull --recurse-submodules
+
+# Update vim and vim modules
 cd ~/.vim && git pull
 cd ~/.vim && git pull --recurse-submodules
 
