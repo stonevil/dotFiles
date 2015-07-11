99d36f171ae753b068163a6838f06d8798415f9b
Myroslav Rys <stonevil@gmail.com>
2015-07-11 04:06:04 +0200

    Remove not required anymore enable.sh and fix install procedure

--- install.sh
+++ install.sh
@@ -6,3 +6,19 @@ if [[ -d $HOME/.Files ]]; then
 else
   cd $HOME && git clone https://github.com/stonevil/dotFiles.git $HOME/.Files
 fi
+
+# Install VIM neobundle.vim plugin
+curl -L https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | /bin/bash -s --
+
+# Install oh-my-zsh and modules
+if [[ -d $HOME/.oh-my-zsh ]]; then
+  cd $HOME && mv -f $HOME/.oh-my-zsh $HOME/.oh-my-zsh.previous
+fi
+cd $HOME && git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
+
+# Install TMUX Plugin Manager
+if [[ -d $HOME/.tmux ]]; then
+  cd $HOME && mv -f $HOME/.tmux $HOME/.tmux.previous
+fi
+cd $HOME && mkdir -p $HOME/.tmux/plugins
+cd $HOME && git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
