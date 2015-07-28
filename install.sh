3db96041a2a375c7754cc720d2fd96b96657f912
Myroslav Rys <stonevil@gmail.com>
2015-07-29 02:04:07 +0300

    Detect shell. Load source file

--- install.sh
+++ install.sh
@@ -6,7 +6,22 @@ if [[ -d $HOME/.Files ]]; then
 else
 	# Clone dotFiles from git repository
 	pushd $HOME && git clone https://github.com/stonevil/dotFiles.git $HOME/.Files && pusdh $HOME/.Files && git pull --recurse-submodules && popd && popd
-	# Install everything
-	source $HOME/conf/.zshrc
-	dotfiles-install-all
 fi
+
+# Read shell config file
+echo "Detecting shell"
+CURRENT_SHELL=`echo $0`
+# Install everything
+echo "Install everything releated"
+case $CURRENT_SHELL in
+	bash)	echo "BASH. Yikes!"
+				source $HOME/conf/.bashrc
+				dotfiles-install-all
+				;;
+	zsh)	echo "ZSH. You cool!"
+				source $HOME/conf/.zshrc
+				dotfiles-install-all
+				;;
+	*)		echo "Not supported shell"
+				;;
+esac
