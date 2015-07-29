f73a302cb7606ab91607208320328ed45e518156
Myroslav Rys <stonevil@gmail.com>
2015-07-29 03:30:30 +0300

    Install procedure fixes. Add create alises function

--- install.sh
+++ install.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/env bash
 
 # Install dotFiles
-if [[ -d $HOME/.Files ]]; then
+if [ -d $HOME/.Files ]; then
 	echo "dotFiles already installed"
 else
-	if [[ `curl -sL -w "%{http_code}\\n" "http://www.google.com/" -o /dev/null` -eq 200 ]]; then
+	if [ `curl -sL -w "%{http_code}\\n" "http://www.google.com/" -o /dev/null` -eq 200 ]; then
 		# Clone dotFiles from git repository
 		pushd $HOME && git clone https://github.com/stonevil/dotFiles.git $HOME/.Files && pushd $HOME/.Files && git pull --recurse-submodules && popd && popd
 	fi
 fi
 
-# Read shell config file
-echo "Install everything releated"
-case "`echo $SHELL | awk -F"/" '{print $NF}'`" in
-	bash)	echo "BASH. Yikes!"
-				source $HOME/.Files/conf/.bashrc && dotfiles-install-all
-				;;
-	zsh)	echo "ZSH. You cool!"
-				source $HOME/.Files/conf/.zshrc && dotfiles-install-all
-				;;
-	*)		echo "Not supported shell"
-				;;
-esac
+if [ `curl -sL -w "%{http_code}\\n" "http://www.google.com/" -o /dev/null` -eq 200 ]; then
+	# Read shell config file
+	echo "Install everything releated"
+	case "`echo $SHELL | awk -F"/" '{print $NF}'`" in
+		bash)	echo "BASH. Yikes!"
+					source $HOME/.Files/conf/.bashrc && dotfiles-install-all && dotfiles-aliases-all
+					;;
+		zsh)	echo "ZSH. You cool!"
+					source $HOME/.Files/conf/.zshrc && dotfiles-install-all && dotfiles-aliases-all
+					;;
+		*)		echo "Not supported shell"
+					;;
+	esac
+fi
