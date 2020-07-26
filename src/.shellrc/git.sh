315bf8ff5e55f1aacc3fa539807c229a8ee81b49
Myroslav Rys <stonevil@gmail.com>
2020-07-26 06:26:29 -0700

    Add git aliases

--- src/.shellrc/git.sh
+++ src/.shellrc/git.sh
@@ -1,7 +1,39 @@
-# vim:ft=sh :
+# vim:ft=sh :
 
 # Git
 if command -v git >/dev/null; then
+	alias gad='git add'
+	alias gAD='git add -A'
+	alias gpu='git push'
+	alias glg='git log'
+	alias gst='git status'
+	alias gdf='git diff'
+	alias gco='git commit -v'
+	alias gca='git commit -a -v'
+	alias gbr='git branch'
+	alias gch='git cheikout'
+	alias gmv='git mv'
+	alias grm='git rm'
+	alias ggr='git grep'
+	alias grad='git remote add'
+	alias grrm='git remote rm'
+	alias gpl='git pull'
+	alias gcl='git clone'
+	alias gta='git tag -a -m'
+	alias gfl='git reflog'
+
+	# leverage an alias from the ~/.gitconfig
+	alias ghi='git hist'
+	alias glg1='git lg1'
+	alias glg2='git lg2'
+	alias glg='git lg'
+
+	# review all files in a git-managed directory that are not yet added to git
+	alias gv='vim $(git ls-files -o -X .gitignore)'
+
+	# cleanup credentials cache
+	alias glogout='git credential-cache exit'
+
 	# review all files in a git-managed directory that are not yet added to git
 	alias gv='vim $(git ls-files -o -X .gitignore)'
 	# cleanup credentials cache
