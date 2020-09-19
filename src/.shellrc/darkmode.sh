408519e08f2702207e115be6c8607dfc0fc55895
Myroslav Rys <stonevil@gmail.com>
2020-09-19 00:00:43 -0700

    Add shellcheck off

--- src/.shellrc/darkmode.sh
+++ src/.shellrc/darkmode.sh
@@ -2,6 +2,7 @@
 # vim:ft=sh :
 
 darkmode() {
+	# shellcheck disable=SC2046,SC2145
 	defaults write $(osascript -e 'id of app "'"$@"'"' | tr -d '\n' | tr -d '\r') NSRequiresAquaSystemAppearance -bool FALSE
 	terminal-notifier -title "The Empire Strikes Back" -subtitle 'Embrace the Dark Side, Luke' -appIcon /System/Library/PrivateFrameworks/PhotoLibraryPrivate.framework/Versions/A/Frameworks/PhotoPrintProduct.framework/Versions/A/Resources/alert-yellow_large.tiff -sound Hero -ignoreDnD -message "Use your $@"
 }
